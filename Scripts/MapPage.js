11bf464117c39e3a5a5c8773fdc6c082ef1de4b9
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-05-22 09:32:21 -0700

    Moved JS and CSS from MapPage.html into separate files.

---
+++ Scripts/MapPage.js
@@ -0,0 +1,32 @@
+﻿require(["dojo/on", "wsdot/extentSelector", "esri/geometry/Extent"], function (on, ExtentSelector, Extent) {
+	var extentSelector, extent;
+
+	/** Retrieves the extent from the hash.  The extent should be provided as four numbers separated by commas.
+	* @returns {esri.geometry.Extent}
+	*/
+	function getInitialExtent() {
+		var extent, re = /#(\-?\d+(?:\.\d+)?),(\-?\d+(?:\.\d+)?),(\-?\d+(?:\.\d+)?),(\-?\d+(?:\.\d+)?)/i, match = location.hash.match(re);
+		if (match) {
+			extent = new Extent({
+				xmin: Number(match[1]),
+				ymin: Number(match[2]),
+				xmax: Number(match[3]),
+				ymax: Number(match[4]),
+				spatialReference: {
+					wkid: 3857
+				}
+			});
+		}
+		return extent;
+	}
+
+	extent = getInitialExtent();
+
+	extentSelector = new ExtentSelector({
+		initExtent: extent
+	});
+	window.extentSelector = extentSelector;
+	on(extentSelector, "extent-change", function () {
+		console.log(this, arguments);
+	});
+});
